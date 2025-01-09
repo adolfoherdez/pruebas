db7301d0632726aedca5c0d06ce67c161d25f3aa
adolfoherdez <150379136+adolfoherdez@users.noreply.github.com>
2025-01-08 22:18:51 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let currentIndex = 0; // Índice de la imagen activa
+const items = document.querySelectorAll('.carousel-item'); // Selecciona todas las imágenes
+
+function showImage(index) {
+    items.forEach(item => item.classList.remove('active')); // Oculta todas las imágenes
+    items[index].classList.add('active'); // Muestra la imagen activa
+}
+
+function changeImage(direction) {
+    currentIndex += direction;
+    if (currentIndex < 0) {
+        currentIndex = items.length - 1;
+    } else if (currentIndex >= items.length) {
+        currentIndex = 0;
+    }
+    showImage(currentIndex);
+}
+
+// Cambio automático cada 5 segundos
+setInterval(() => changeImage(1), 5000);
+
+// Mostrar la primera imagen al cargar
+showImage(currentIndex);
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const header = document.querySelector(".header");
+
+    menuToggle.addEventListener("click", () => {
+        header.classList.toggle("active"); // Agrega o quita la clase 'active'
+    });
+});
+
+    
